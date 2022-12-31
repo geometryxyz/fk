a6b28ad96d0092a711f741432786dcc23362fa82
Kobi Gurkan <kobigurk@gmail.com>
2022-12-31 19:54:36 +0200

    feat: adds basic CD

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,23 @@ use ark_std::log2;
 pub use toeplitz::Toeplitz;
 
 pub fn next_pow2(n: usize) -> usize {
-    let two: u32 = 2; 
+    let two: u32 = 2;
     let a: u32 = log2(n);
 
     if two.pow(a - 1) == n as u32 {
         return n;
     }
 
-    return two.pow(a).try_into().unwrap()
+    two.pow(a).try_into().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use std::iter;
 
-    use ark_bn254::{Fr, G1Affine, G1Projective, Bn254};
+    use ark_bn254::{Bn254, Fr, G1Affine, G1Projective};
     use ark_ec::{msm::VariableBaseMSM, AffineCurve, PairingEngine};
-    use ark_ff::{One, PrimeField, Field, Zero, UniformRand};
+    use ark_ff::{Field, One, PrimeField, UniformRand, Zero};
     use ark_poly::{
         univariate::DensePolynomial, EvaluationDomain, GeneralEvaluationDomain, Polynomial,
         UVPolynomial,
@@ -59,7 +59,7 @@ mod tests {
             );
         }
         let proof: E::G1Affine = commit::<E>(srs, &q).into();
-        (poly.evaluate(&challenge), proof.into())
+        (poly.evaluate(&challenge), proof)
     }
 
     fn commit_in_each_omega_i<E: PairingEngine>(
@@ -67,7 +67,8 @@ mod tests {
         domain: &GeneralEvaluationDomain<E::Fr>,
         poly: &DensePolynomial<E::Fr>,
     ) -> Vec<E::G1Affine> {
-        domain.elements()
+        domain
+            .elements()
             .map(|omega_pow_i| open::<E>(srs, poly, omega_pow_i).1)
             .collect()
     }
@@ -128,7 +129,11 @@ mod tests {
 
         let d = 5;
         let next_pow_2_deg = next_pow2(d);
-        let mut srs_proj: Vec<G1Projective> = srs.iter().take(next_pow_2_deg).map(|t| t.into_projective()).collect();
+        let mut srs_proj: Vec<G1Projective> = srs
+            .iter()
+            .take(next_pow_2_deg)
+            .map(|t| t.into_projective())
+            .collect();
         srs_proj.reverse();
 
         let poly = DensePolynomial::<Fr>::rand(d, &mut rng);
@@ -147,22 +152,21 @@ mod tests {
 
     #[test]
     fn test_poly_correctness() {
-        let mut rng = test_rng(); 
-        let n = 4; 
+        let mut rng = test_rng();
+        let n = 4;
 
-        let p = DensePolynomial::<Fr>::rand(3, &mut rng); 
+        let p = DensePolynomial::<Fr>::rand(3, &mut rng);
         let domain = GeneralEvaluationDomain::<Fr>::new(n).unwrap();
 
         let omega = domain.element(1);
         let omega_squared = domain.element(2);
 
-
         let q1 = &p / &DensePolynomial::from_coefficients_slice(&[-omega, Fr::one()]);
 
         let q1_by_hand = DensePolynomial::from_coefficients_slice(&[
-            p.coeffs[1] + omega * p.coeffs[2] + omega_squared*p.coeffs[3],
+            p.coeffs[1] + omega * p.coeffs[2] + omega_squared * p.coeffs[3],
             omega * p.coeffs[3] + p.coeffs[2],
-            p.coeffs[3], 
+            p.coeffs[3],
         ]);
 
         assert_eq!(q1, q1_by_hand);
@@ -170,28 +174,32 @@ mod tests {
 
     #[test]
     fn test_h_polys() {
-        let mut rng = test_rng(); 
-        let n = 64; 
+        let mut rng = test_rng();
+        let n = 64;
 
-        let p = DensePolynomial::<Fr>::rand(n, &mut rng); 
+        let p = DensePolynomial::<Fr>::rand(n, &mut rng);
         let domain = GeneralEvaluationDomain::<Fr>::new(n).unwrap();
 
         let omega = domain.element(1);
         let q1 = &p / &DensePolynomial::from_coefficients_slice(&[-omega, Fr::one()]);
 
-        let tau = Fr::from(15); 
+        let tau = Fr::from(15);
         let q1_eval = q1.evaluate(&tau);
 
         let mut h_coeffs = p.coeffs()[..].to_vec();
 
-        let hs: Vec<DensePolynomial<_>> = (0..n).map(|_| {
-            h_coeffs.remove(0);
-            DensePolynomial::from_coefficients_slice(&h_coeffs)
-        }).collect();
+        let hs: Vec<DensePolynomial<_>> = (0..n)
+            .map(|_| {
+                h_coeffs.remove(0);
+                DensePolynomial::from_coefficients_slice(&h_coeffs)
+            })
+            .collect();
 
-        let h_sum: Fr = hs.iter().enumerate().map(|(i, hi)| {
-            hi.evaluate(&tau) * omega.pow([i as u64])
-        }).sum();
+        let h_sum: Fr = hs
+            .iter()
+            .enumerate()
+            .map(|(i, hi)| hi.evaluate(&tau) * omega.pow([i as u64]))
+            .sum();
 
         assert_eq!(q1_eval, h_sum);
     }
